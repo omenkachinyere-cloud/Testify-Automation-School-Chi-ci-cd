1647d6cd5c93a0022b269e43794e2239662430d9
Chiqa <omenkachinyere@gmail.com>
2026-03-01 16:37:36 +0100

    add unitest

---
+++ testy.py
@@ -0,0 +1,15 @@
+
+import main
+import unittest
+
+class Testmain(unittest.TestCase):
+
+    def test_addition(self):
+        self.assertEqual(main.addition(1, 2), 3, "should be 3")
+        self.assertEqual(main.addition(5, 5), 10, "should be 10")
+        self.assertEqual(main.addition(40, 20), 60, "should be 60")
+        self.assertEqual(main.addition(-3, 2), -1, "should be -1")
+
+if _name_ =='_main_':
+    unittest.main()
+    
