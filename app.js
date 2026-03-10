92e774a2658402ee59f97598f4c9dae15492cc9f
Chiqa <omenkachinyere@gmail.com>
2026-03-10 11:33:50 +0100

    app.js

--- app.js
+++ app.js
@@ -59,9 +59,53 @@ const position = 5;
 const output = 'Today is ' + dayOfTheWeek  +'.'  + ' It is the ' + position + 'th' + ' day of the week.';
 console.log(output);
 
+const addition = -5 + 20;
+console.log(addition); // logs the result of the addition to the console
 
+const subtraction = 340 - 5;
+console.log(subtraction); // logs the result of the subtraction to the console
 
+const division = 100 / 5;
+console.log(division); // logs the result of the division to the console
 
+const multiplication = 3114 * 54;
+console.log(multiplication); // logs the result of the multiplication to the console
 
+const modulus = 10 % 3;
+console.log(modulus); // logs the result of the modulus operation to the console
+
+const modulus2 = 36 % 4;
+console.log(modulus2); // logs the result of the modulus operation to the console
+
+const compare = 31 > 30.99;
+console.log(compare); // logs the result of the comparison to the console
+
+const compare2 = 3 > 30.99;
+console.log(compare2); // logs the result of the comparison to the console
+
+console.log(6==='6'); // logs the result of the comparison to the console
+
+console.log(6==6); // logs the result of the comparison to the console
+
+console.log(6+ + '6'); // logs the result of the comparison to the console
+console.log(typeof(6 +'6')); // logs the result of the comparison to the console
+console.log(7 +'birds'); // logs the result of the comparison to the console
+console.log(7 - 'birds'); // logs the result of the comparison to the console
+
+const age = 20;
+console.log(30<20); // logs the result of the logical AND operation to the console
+console.log(30>=age); // logs the result of the logical AND operation to the console
+/*
+AND Operator ---> && all must be true for the result to be true
+OR Operator ---> || either one must be true for the result to be true
+NOT Operator ---> ! negates the value of a boolean expression, if the expression is true, it becomes false, and if it is false, it becomes true.
+*/ 
+const MaritalStatus = 'Married';
+const hasChildren = 4;
+//const logic = MaritalStatus === 'Married' && hasChildren === 4;
+
+const logic = MaritalStatus === 'Married' || hasChildren === 4;
+
+console.log(!logic);
 
 
